e7fabdf3058996944576d118128d1ae14c5d9b55
ceoselectdine <sjavid.z2014@vit.ac.in>
2016-10-18 08:14:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cardsandlists/Person.java
@@ -0,0 +1,17 @@
+package com.example.cardsandlists;
+
+/**
+ * Created by jeevansai on 18/10/2016.
+ */
+
+class Person {
+    String name;
+    String age;
+    int photoId;
+
+    Person(String name, String age, int photoId) {
+        this.name = name;
+        this.age = age;
+        this.photoId = photoId;
+    }
+}
